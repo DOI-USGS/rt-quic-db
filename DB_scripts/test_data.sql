795b063bac151842918e084cc67222d2045fd4bb
Nathan Bollig <nbollig@wisc.edu>
2020-06-25 15:47:10 -0500

    Part of last commit

--- DB_scripts/test_data.sql
+++ DB_scripts/test_data.sql
@@ -1,3 +1,8 @@
-INSERT INTO `rt_quic_db`.`USERS` (`NAME`, `ROLE`, `USERNAME`, `PASSWORD`) VALUES ('Chit', 'admin', 'chit', 'chit');
-INSERT INTO `rt_quic_db`.`USERS` (`NAME`, `ROLE`, `USERNAME`, `PASSWORD`) VALUES ('Nathan', 'admin', 'nathan', 'nathan');
-INSERT INTO `rt_quic_db`.`USERS` (`NAME`, `ROLE`, `USERNAME`, `PASSWORD`) VALUES ('Jojo', 'admin', 'jojo', 'jojo');
+INSERT INTO `rt_quic_db`.`Users` (`NAME`, `ROLE`, `USERNAME`, `PASSWORD`) VALUES ('Chit', 'admin', 'chit', 'chit');
+INSERT INTO `rt_quic_db`.`Users` (`NAME`, `ROLE`, `USERNAME`, `PASSWORD`) VALUES ('Nathan', 'admin', 'nathan', 'nathan');
+INSERT INTO `rt_quic_db`.`Users` (`NAME`, `ROLE`, `USERNAME`, `PASSWORD`) VALUES ('Jojo', 'admin', 'jojo', 'jojo');
+
+INSERT INTO `rt_quic_db`.`Plate` (`plate_ID`, `plate_type`, `other_plate_attr`) VALUES (99, 'Test_Plate', NULL)
+INSERT INTO `rt_quic_db`.`Sample` (`sample_ID`, `name`, `species`, `sex`, `age`, `tissue_matrix`, `other_sample_attr`) 
+	VALUES (99, 'Test_Sample', 'Bovine', 'F', 5, 'CSF', NULL);
+INSERT INTO `rt_quic_db`.`Location` (`loc_ID`, `name`) VALUES (99, 'Test_Location')
