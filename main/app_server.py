0bbdd6bf2f1135b51718bf891baa28035a8c6476
hged13 <91993290+hged13@users.noreply.github.com>
2022-07-09 13:08:00 -0600

    Added value 'preparation_method' to database and website forms.

--- main/app_server.py
+++ main/app_server.py
@@ -951,6 +951,6 @@ def create_issue(title, body, label):
 
 if __name__ == '__main__':
     os.environ["FLASK_ENV"] = 'development'
-    port = int(os.environ.get('PORT', 55556))
+    port = int(os.environ.get('PORT', 55555))
     #login_manager.init_app(app)
     app.run(host='0.0.0.0', port=port, use_reloader=False, debug=True)
